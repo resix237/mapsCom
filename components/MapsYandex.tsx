699f50e84ddd9643f94d469d70415c5dd107519e
Murielleloe <therese.loe@abyster.com>
2023-06-30 11:55:41 +0100

    update maps

--- components/MapsYandex.tsx
+++ components/MapsYandex.tsx
@@ -1,23 +1,27 @@
 import { useState } from 'react';
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import { YMaps, Map, Placemark, GeolocationControl, ZoomControl } from '@pbe/react-yandex-maps';
 interface Position {
     name: string;
     coordinates: number[];
+    proprities: any
 }
 
-function MapsYandex({ positions, markerColor }: any) {
+function MapsYandex({ positions, markerColor, width, height }: any) {
     return (
-        <YMaps query={{ lang: 'en_US' }}>
-            <Map defaultState={{ center: positions[0]?.coordinates, zoom: 8 }}>
+        <YMaps query={{ lang: 'en_US' }} >
+            <Map defaultState={{ center: positions[0]?.coordinates, zoom: 8 }} width={width} height={height}>
                 {positions.map((position: Position, index: number) => (
                     <Placemark
                         key={index}
                         geometry={position.coordinates}
-                        options={{ iconColor: markerColor }}
-                        properties={{ iconContent: position.name }}
+                        options={{ iconColor: markerColor, balloonContent: '<strong>crocodile\'s nose </strong> color' }}
+                        properties={{}}
+
 
                     />
                 ))}
+                <GeolocationControl options={{ float: "left" }} />
+                <ZoomControl options={{ float: "right" }} />
             </Map>
         </YMaps>
     );
--- pages/maps.tsx
+++ pages/maps.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 import MapsYandex from '@/components/MapsYandex';
-
 const positions = [
-    { name: 'Position', coordinates: [53.751244, 37.618423] },
-    { name: 'Position 2', coordinates: [52.751456, 37.617822] },
-    { name: 'Position 3', coordinates: [50.751687, 37.617331] },
+    { name: 'Position', coordinates: [3.850908, 11.511931] },
+    { name: 'Position 2', coordinates: [3.891000, 11.512411] },
+    { name: 'Position 3', coordinates: [3.871000, 11.512411] },
 ];
 
 function maps() {
     return (
         <div>
             <h1>Carte avec marqueurs</h1>
-            <div>
-                <MapsYandex positions={positions} markerColor="blue" />
-            </div>
+
+            <MapsYandex positions={positions} markerColor="blue" width={500} height={500} />
         </div>
     )
 }
