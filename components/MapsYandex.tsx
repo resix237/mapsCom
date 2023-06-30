518a1474f57bdad96d235ef37df039060baafbf8
Murielleloe <therese.loe@abyster.com>
2023-06-30 13:36:39 +0100

    update maps

--- components/MapsYandex.tsx
+++ components/MapsYandex.tsx
@@ -1,28 +1,50 @@
 import { useState } from 'react';
-import { YMaps, Map, Placemark, GeolocationControl, ZoomControl } from '@pbe/react-yandex-maps';
+import { YMaps, Map, Placemark, GeolocationControl, ZoomControl, Clusterer } from '@pbe/react-yandex-maps';
 interface Position {
     name: string;
     coordinates: number[];
-    proprities: any
+    properties: {
+        balloonContent?: string;
+    };
 }
 
 function MapsYandex({ positions, markerColor, width, height }: any) {
+    const [selectedPlacemark, setSelectedPlacemark] = useState<Position | null>(null);
+
+    const handlePlacemarkClick = (placemark: any) => {
+        setSelectedPlacemark(placemark);
+    };
+    const handleBalloonClose = () => {
+        setSelectedPlacemark(null);
+    };
     return (
         <YMaps query={{ lang: 'en_US' }} >
             <Map defaultState={{ center: positions[0]?.coordinates, zoom: 8 }} width={width} height={height}>
+
                 {positions.map((position: Position, index: number) => (
                     <Placemark
                         key={index}
                         geometry={position.coordinates}
                         options={{ iconColor: markerColor, balloonContent: '<strong>crocodile\'s nose </strong> color' }}
-                        properties={{}}
+                        properties={position.properties}
+                        onClick={() => handlePlacemarkClick(position)}
 
 
                     />
                 ))}
+
                 <GeolocationControl options={{ float: "left" }} />
                 <ZoomControl options={{ float: "right" }} />
+
+
+                {selectedPlacemark && (
+                    <div className="custom-popup custom-popup absolute bg-white p-4 rounded shadow">
+                        <h4 className='text-lg font-bold mb-2'>{selectedPlacemark.name}</h4>
+                        <p>{selectedPlacemark.properties?.balloonContent}</p>
+                    </div>
+                )}
             </Map>
+
         </YMaps>
     );
 }
--- pages/maps.tsx
+++ pages/maps.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 import MapsYandex from '@/components/MapsYandex';
+import Mapp from '../components/Mapp';
+
 const positions = [
-    { name: 'Position', coordinates: [3.850908, 11.511931] },
-    { name: 'Position 2', coordinates: [3.891000, 11.512411] },
-    { name: 'Position 3', coordinates: [3.871000, 11.512411] },
+    {
+        name: 'Position', coordinates: [3.850908, 11.511931], properties: {
+            balloonContent: '<strong>crocodile\'s nose </strong> color'
+        }
+    },
+    {
+        name: 'Position 2', coordinates: [3.891000, 11.512411], properties: {
+            balloonContent: '<strong>crocodile\'s nose </strong> color'
+        }
+    },
+    {
+        name: 'Position 3', coordinates: [3.871000, 11.512411], properties: {
+            balloonContent: '<strong>crocodile\'s nose </strong> color'
+        }
+    },
 ];
 
 function maps() {
@@ -13,6 +27,7 @@ function maps() {
             <h1>Carte avec marqueurs</h1>
 
             <MapsYandex positions={positions} markerColor="blue" width={500} height={500} />
+            {/* <Mapp /> */}
         </div>
     )
 }
